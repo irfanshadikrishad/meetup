099ded876fb3b0211f20c2cccfb96368d55bce86
Mahbubaly <mahbubalikhanofficial@gmail.com>
2024-12-09 01:55:48 +0600

    fixing errors

--- app/home/page.js
+++ app/home/page.js
@@ -7,10 +7,11 @@ import { useAuth } from "../../store/auth";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Container from "../../components/Container";
+import { MdDelete } from "react-icons/md";
 
 const Page = () => {
     const { user, authenticate } = useAuth();
-    const [slots, setSlots] = useState([]);
+    const [slots, setSlots] = useState([])
     console.log(user);
 
     const [loggedInUser, setLoggedInUser] = useState({});
@@ -56,9 +57,8 @@ const Page = () => {
                 toast.success(`Slot created successfully.`)
                 getUsersSlots()
             } else {
-                toast.error(`Error creating slot.`)
+                toast.error(`Error creating slot.`);
             }
-
         } catch (error) {
             console.log("Error during API call:", error);
             alert("An error occurred while creating the slot.");
@@ -75,10 +75,10 @@ const Page = () => {
                 });
                 const response = await request.json();
                 if (request.status === 200) {
-                    setSlots(response);
+                    setSlots(response)
                 } else {
                     console.log(response);
-                    toast.error(response.error);
+                    toast.error(response.error)
                 }
             } else {
                 console.log(`user id missed`);
@@ -102,130 +102,158 @@ const Page = () => {
         return <div>Loading...</div>; // Render a loading state while user information is being fetched
     }
 
+
     console.log(user.role);
+    console.log(slots);
 
     return user.role === "host" ? (
-        <div className="bg-[#ffffff] min-h-screen flex justify-center items-center">
-            <div className="flex flex-col m-10 gap-6 md:flex-row w-full max-w-4xl items-center">
-                {/* Image Section */}
-                <div className="w-full md:w-1/2 flex -ml-[50px]">
-                    {/* Cards */}
-                    <div style={{ display: "grid", gap: "5px" }}>
-                        {slots.length > 0 && slots.map((slo, idx) => {
-                            return <Link href={`/slot/${slo.id}`} className="w-[full] rounded-[5px] bg-primary text-black p-10" key={idx} >
-                                <p className="text-[18px] font-bold">Host Name: {slo.name}</p>
-                                <p className="text-[18px] font-semibold">Event id: {slo.id}</p>
-                            </Link>
-                        })}
-                    </div>
-                </div>
+        <>
 
-                {/* Form Section */}
-                <div className="w-full md:w-1/2 flex flex-col p-6 rounded-md sm:p-10 bg-gray-100 text-gray-900">
-                    <div className="mb-8 text-center">
-                        <h1 className="my-3 text-4xl font-bold">Create a Slot</h1>
-                        <p className="text-sm text-gray-400">
-                            Fill in the details to create a slot.
-                        </p>
-                    </div>
+            <Container>
 
-                    <form onSubmit={handleSubmit} className="space-y-6">
-                        {/* Host Name */}
-                        <div>
-                            <label htmlFor="hostName" className="block mb-2 text-sm">
-                                Host Name
-                            </label>
-                            <input
-                                type="text"
-                                name="hostName"
-                                value={loggedInUser && loggedInUser.name ? loggedInUser.name : ""}
-                                disabled
-                                required
-                                placeholder="Enter Host Name"
-                                className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-[#41a6d4] bg-gray-200 text-gray-900"
-                            />
+
+                <div className="bg-[#ffffff] my-10">
+                    <div className="flex flex-col  gap-6 md:flex-row w-full   ">
+                        {/* Image Section */}
+                        <div className="w-full md:w-1/2 ">
+                            {/* Cards */}
+                            <div style={{ display: "grid", gap: "5px" }}>
+                                {slots.length > 0 && slots.map((slo, idx) => {
+                                    return <div className="w-[full] rounded-[5px] bg-secondary h-[200px] text-black p-6" key={idx} >
+                                        <p className="text-[18px] font-bold">Host Name: {slo.name}</p>
+                                        <p className="text-[18px] font-bold">Event date: {slo.date}</p>
+                                        <p className="text-[18px] font-bold">Event id: {slo.time_limit} min</p>
+
+                                        <p className="text-[18px] font-semibold">Event id: {slo.id}</p>
+
+
+                                        <div className="flex  items-center justify-between">
+
+                                            <div>
+
+                                                <button className="p-2 mt-2 bg-[#A594F9] text-black font-bold rounded-[5px]"> <Link href={`/slot/${slo.id}`}>View details</Link></button>
+
+                                            </div>
+
+
+                                            <div>
+                                                <button>  <MdDelete className="text-[28px] text-[#ff2f2f]" /></button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                })}
+                            </div>
                         </div>
 
-                        {/* Slot Date */}
-                        <div>
-                            <label htmlFor="slotDate" className="block mb-2 text-sm">
-                                Slot Date
-                            </label>
-                            <input
-                                type="date"
-                                name="slotDate"
-                                value={formData.slotDate}
-                                onChange={handleInputChange}
-                                required
-                                className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-[#41a6d4] bg-gray-200 text-gray-900"
-                            />
-                        </div>
+                        {/* Form Section */}
+                        <div className="w-full md:w-1/2 flex flex-col h-[700px]  rounded-md sm:p-10 bg-gray-100 text-gray-900">
+                            <div className="mb-8 text-center">
+                                <h1 className="my-3 text-4xl font-bold">Create a Slot</h1>
+                                <p className="text-sm text-gray-400">
+                                    Fill in the details to create a slot.
+                                </p>
+                            </div>
 
-                        {/* Slot Time Limit */}
-                        <div>
-                            <label htmlFor="slotTimeLimit" className="block mb-2 text-sm">
-                                Slot Time Limit (in minutes)
-                            </label>
-                            <input
-                                type="number"
-                                name="slotTimeLimit"
-                                value={formData.slotTimeLimit}
-                                onChange={handleInputChange}
-                                required
-                                placeholder="Enter Time Limit"
-                                className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-[#41a6d4] bg-gray-200 text-gray-900"
-                            />
-                        </div>
-
-                        {/* Slot User Limit */}
-                        <div>
-                            <label htmlFor="slotUserLimit" className="block mb-2 text-sm">
-                                Slot User Limit
-                            </label>
-                            <input
-                                type="number"
-                                name="slotUserLimit"
-                                value={formData.slotUserLimit}
-                                onChange={handleInputChange}
-                                required
-                                placeholder="Enter User Limit"
-                                className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-[#41a6d4] bg-gray-200 text-gray-900"
-                            />
-                        </div>
-
-                        <button
-                            type="submit"
-                            className="bg-[#41a6d4] hover:bg-[#4185a4] font-bold w-full rounded-[25px] py-2 text-white"
-                        >
-                            Submit
-                        </button>
-                    </form>
-
-                    <Link href="/">
-                        <div className="flex justify-center mx-auto mt-[10px]">
-                            <button className="flex items-center justify-center w-1/2 px-5 py-1 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto hover:bg-gray-100">
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    strokeWidth="1.5"
-                                    stroke="currentColor"
-                                    className="w-5 h-5 rtl:rotate-180 text-rose-500"
-                                >
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
+                            <form onSubmit={handleSubmit} className="space-y-6">
+                                {/* Host Name */}
+                                <div>
+                                    <label htmlFor="hostName" className="block mb-2 text-sm">
+                                        Host Name
+                                    </label>
+                                    <input
+                                        type="text"
+                                        name="hostName"
+                                        value={loggedInUser && loggedInUser.name ? loggedInUser.name : ""}
+                                        disabled
+                                        required
+                                        placeholder="Enter Host Name"
+                                        className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-[#41a6d4] bg-gray-200 text-gray-900"
                                     />
-                                </svg>
-                                <span>Go back</span>
-                            </button>
+                                </div>
+
+                                {/* Slot Date */}
+                                <div>
+                                    <label htmlFor="slotDate" className="block mb-2 text-sm">
+                                        Slot Date
+                                    </label>
+                                    <input
+                                        type="date"
+                                        name="slotDate"
+                                        value={formData.slotDate}
+                                        onChange={handleInputChange}
+                                        required
+                                        className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-[#41a6d4] bg-gray-200 text-gray-900"
+                                    />
+                                </div>
+
+                                {/* Slot Time Limit */}
+                                <div>
+                                    <label htmlFor="slotTimeLimit" className="block mb-2 text-sm">
+                                        Slot Time Limit (in minutes)
+                                    </label>
+                                    <input
+                                        type="number"
+                                        name="slotTimeLimit"
+                                        value={formData.slotTimeLimit}
+                                        onChange={handleInputChange}
+                                        required
+                                        placeholder="Enter Time Limit"
+                                        className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-[#41a6d4] bg-gray-200 text-gray-900"
+                                    />
+                                </div>
+
+                                {/* Slot User Limit */}
+                                <div>
+                                    <label htmlFor="slotUserLimit" className="block mb-2 text-sm">
+                                        Slot User Limit
+                                    </label>
+                                    <input
+                                        type="number"
+                                        name="slotUserLimit"
+                                        value={formData.slotUserLimit}
+                                        onChange={handleInputChange}
+                                        required
+                                        placeholder="Enter User Limit"
+                                        className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-[#41a6d4] bg-gray-200 text-gray-900"
+                                    />
+                                </div>
+
+                                <button
+                                    type="submit"
+                                    className="bg-[#41a6d4] hover:bg-[#4185a4] font-bold w-full rounded-[25px] py-2 text-white"
+                                >
+                                    Submit
+                                </button>
+                            </form>
+
+                            <Link href="/">
+                                <div className="flex justify-center mx-auto mt-[10px]">
+                                    <button className="flex items-center justify-center w-1/2 px-5 py-1 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto hover:bg-gray-100">
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            strokeWidth="1.5"
+                                            stroke="currentColor"
+                                            className="w-5 h-5 rtl:rotate-180 text-rose-500"
+                                        >
+                                            <path
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
+                                            />
+                                        </svg>
+                                        <span>Go back</span>
+                                    </button>
+                                </div>
+                            </Link>
                         </div>
-                    </Link>
+                    </div>
+                    <ToastContainer />
                 </div>
-            </div>
-            <ToastContainer />
-        </div>
+            </Container>
+
+        </>
     ) : (
 
         <div>You are not authorized to create a slot.</div>
