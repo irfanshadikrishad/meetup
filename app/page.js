e89b460279d25640b7ebca949e1df685f8b43a0c
Mahbubaly <mahbubalikhanofficial@gmail.com>
2024-12-08 11:57:03 +0600

    sign up page

--- app/page.js
+++ app/page.js
@@ -1,3 +1,42 @@
-export default function Home(){
-  return
-}
+import React from 'react';
+import Image from 'next/image';
+import logo from '../app/Image/logo.png';
+
+const Page = () => {
+  return (
+
+    <div className='bg-[#DDFFAB]'>
+
+      <div>
+        <div className="flex items-center ">
+          <Image
+            src={logo}
+            alt="Logo"
+            width={45}
+            height={50}
+          />
+          <h3 className="text-lg font-semibold">eet Up</h3>
+        </div>
+
+
+
+        <div>
+
+        </div>
+
+
+
+
+      </div>
+
+
+
+
+
+
+
+    </div>
+  );
+};
+
+export default Page;
